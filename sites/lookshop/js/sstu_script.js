e13c3b820a617d217ab997b5865ee2cadd497014
trumanpvt <37899377+trumanpvt@users.noreply.github.com>
2018-05-18 13:48:49 +0300

    Add files via upload

---
+++ sites/lookshop/js/sstu_script.js
@@ -0,0 +1,10 @@
+jQuery(function($) {
+	$(".header-main_swap-menu").click(function (){
+		$(".header-main_menu_ul").toggle();
+		return false;
+	});
+		$(".footer_swap-menu").click(function (){
+		$(".footer_menu_ul").toggle();
+		return false;
+	});
+})
