824dea1f2a8b278ac5faad0c73db0ff01c60f10f
Andrews <trumanpvt@gmail.com>
2018-05-28 19:33:40 +0300

    index changed

--- sites/pink/js/sstu_script.js
+++ sites/pink/js/sstu_script.js
@@ -2,14 +2,14 @@ jQuery(function($) {
     $(".header-top__swap-menu").click(function() {
         $(".header-top__nav__opened").toggle();
         $(".header-top__swap-menu").toggle();
-        document.getElementById("header-top").style.backgroundColor = "#283645";
+        document.getElementById("header-top__menu").style.backgroundColor = "#283645";
         return false;
     });
     $(".header-top__swap-menu-opened").click(function() {
         $(".header-top__nav__opened").toggle();
         $(".header-top__swap-menu").toggle();
-        document.getElementById("header-top").style.backgroundColor = "rgba(29, 38, 49, 0.7)";
-        document.getElementById("header-top").style.borderBottom = "none";
+        document.getElementById("header-top__menu").style.backgroundColor = "rgba(29, 38, 49, 0.7)";
+        document.getElementById("header-top__menu").style.borderBottom = "none";
         return false;
     });
 })
