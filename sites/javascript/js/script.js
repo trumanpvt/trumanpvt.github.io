143dd66b1b313be59103d2592733d1255f091cfe
Andrews <trumanpvt@gmail.com>
2018-06-05 19:02:03 +0300

    IE support fixed

--- sites/javascript/js/script.js
+++ sites/javascript/js/script.js
@@ -1,8 +1,5 @@
-var user = { name: 'Вася'};
-var name = 'Dude';
-var nameAge = name + 4;
-var x = -1;
-x = -x;
-var string = 'Моя строка';
-string = string - 'а';
-alert(string);
+var x;
+
+false || (x = 1);
+
+alert(x);
