4f1176b7cb6148be3fadfed8b6bb2154384a71b9
Andrews <trumanpvt@gmail.com>
2018-06-09 18:09:24 +0300

    fonts fixed

--- sites/javascript/js/script.js
+++ sites/javascript/js/script.js
@@ -1,1 +1,12 @@
-alert( String(null) === "null" );
+function fib(n) {
+	if (n == 1 || n == 2) return 1;
+    for (var a = 1, b = 1, i = 3; i <= n; i++) {
+        var c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return c;
+}
+
+alert(fib(2));
