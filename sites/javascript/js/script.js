dacfd8b6a5fe578c970ae4c122e81bb703bcb162
Andrews <trumanpvt@gmail.com>
2018-06-21 17:44:30 +0300

    pink slider swipe added

--- sites/javascript/js/script.js
+++ sites/javascript/js/script.js
@@ -1,12 +1,5 @@
-function fib(n) {
-	if (n == 1 || n == 2) return 1;
-    for (var a = 1, b = 1, i = 3; i <= n; i++) {
-        var c = a + b;
-        a = b;
-        b = c;
-    }
+var userName = prompt("Введите имя?", "Василий");
+var isTeaWanted = confirm("Вы хотите чаю?");
 
-    return c;
-}
-
-alert(fib(2));
+alert( "Посетитель: " + userName );
+alert( "Чай: " + isTeaWanted );
