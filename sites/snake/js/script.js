a4e3ee8765f6c8e66155c47fd981a7486ae15ce6
Andrews <trumanpvt@gmail.com>
2018-08-10 17:39:13 +0300

    snake changed

--- sites/snake/js/script.js
+++ sites/snake/js/script.js
@@ -35,25 +35,34 @@ function moveSnake(e) {
     switch (e.keyCode) {
 
         case 37: // если нажата клавиша влево
-            if (leftCoord > 5)
+            if (leftCoord > 5) {
+            snake.style.transform = 'rotate(90deg)';
+            // while (leftCoord > 5 && e.keyCode != 38 && e.keyCode != 39 && e.keyCode != 40) {
                 snake.style.left = leftCoord - 4 + "px";
-            leftCoord -= 4;
+                leftCoord -= 4;
 
+            }
             break;
         case 38: // если нажата клавиша вверх
-            if (topCoord > 5)
+            if (topCoord > 5) {
+                snake.style.transform = 'rotate(-180deg)';
                 snake.style.top = topCoord - 4 + "px";
-            topCoord -= 4;
+                topCoord -= 4;
+            }
             break;
         case 39: // если нажата клавиша вправо
-            if (leftCoord < 315)
+            if (leftCoord < 295) {
+                snake.style.transform = 'rotate(-90deg)';
                 snake.style.left = leftCoord + 4 + "px";
-            leftCoord += 4
+                leftCoord += 4
+            }
             break;
         case 40: // если нажата клавиша вниз
-            if (topCoord < document.documentElement.clientHeight - 100)
+            if (topCoord < 295) {
+                snake.style.transform = 'unset';
                 snake.style.top = topCoord + 4 + "px";
-            topCoord += 4;
+                topCoord += 4;
+            }
             break;
     }
 }
