8330250abe9f1e510ff0e59cbebb2af9acd81c53
trumanpvt <trumanpvt@gmail.com>
2018-09-11 13:11:23 +0300

    tvzavr changed

--- sites/tvzavr/js/script.js
+++ sites/tvzavr/js/script.js
@@ -1,22 +1,55 @@
 'use strict';
 
+var carousel = document.getElementsByClassName('carousel');
+var divs = carousel[0].getElementsByTagName('*');
+var currentSlide = 1;
+var length = divs.length;
+
+function slideLeft() {
+
+    if (currentSlide > 0) {
+        divs[currentSlide].classList.remove('focused');
+        divs[currentSlide -= 1].classList.add('focused');
+
+        if (currentSlide - 1 >= 0) {
+            divs[currentSlide - 1].style.display = 'block';
+        }
+    }
+    console.log('left');
+    console.log('currentSlide: ' + currentSlide);
+    console.log('length: ' + length);
+}
+
+function slideRight() {
+
+    if (currentSlide < length - 1) {
+        divs[currentSlide].classList.remove('focused');
+        divs[currentSlide += 1].classList.add('focused');
+
+        if (currentSlide + 2 < length) {
+            divs[currentSlide - 2].style.display = 'none';
+        }
+    }
+
+    console.log('right');
+    console.log('currentSlide: ' + currentSlide);
+    console.log('length: ' + length);
+}
+
 
 function slider(e) {
 
-    var snake = document.getElementById("snake");
-    var cs = window.getComputedStyle(snake);
-
     switch (e.keyCode) {
 
-        case 37: // если нажата клавиша влево
-          
+        case 37:
+            slideLeft();
             break;
 
-        case 39: // если нажата клавиша вправо
-
+        case 39:
+            slideRight();
             break;
 
     }
 }
 
-addEventListener("keydown", slider);
+addEventListener('keydown', slider);
